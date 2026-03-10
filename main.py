dd2a92662a4175d1c23fec3e32864ba988f89a75
Ivan Manojlovic <spidshine@gmail.com>
2026-03-10 19:56:35 +0100

    Dodate funkcionalnosti: uklanjanje proizvoda i klasa Cart sa logikom za nasumična 3 komada

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
+import random
 
-# Kreiranje ProductManager‑a
+# Kreiranje ProductManager-a
 manager = ProductManager()
 
-# Dodavanje nekoliko proizvoda
+# Dodavanje proizvoda u inventar
 manager.add_product(Product("Laptop", 50000, 2))
 manager.add_product(Product("Telefon", 20000, 3))
 manager.add_product(Product("Slusalice", 3000, 10))
+manager.add_product(Product("Tastatura", 1000, 5))
+manager.add_product(Product("Mis", 500, 7))
 
-# Prikaz svih proizvoda
 print("Lista proizvoda:")
 manager.display_products()
-
-# Prikaz ukupne vrednosti inventara
 print("Ukupna vrednost inventara:", manager.total_value(), "RSD")
 
-# Uklanjanje proizvoda
+# Test remove_product - uklanjanje jednog proizvoda iz inventara
 manager.remove_product("Telefon")
-print("\nNakon uklanjanja proizvoda:")
+print("\nNakon uklanjanja telefona:")
 manager.display_products()
+
+# Kreiranje liste svih komada (svaki quantity se razbije na pojedinačne proizvode)
+all_items = []
+for product in manager.products:
+    for _ in range(product.quantity):
+        all_items.append(Product(product.name, product.price, 1))
+
+# Kreiranje korpe
+cart = Cart()
+
+# Dodavanje 3 slučajno odabrana komada u korpu
+for item in random.sample(all_items, 3):
+    cart.add_to_cart(item)
+
+# Prikaz sadržaja korpe
+print("\nSadržaj korpe:")
+cart.display_cart()
+print("Ukupna vrednost korpe:", cart.total_cart_value(), "RSD")
